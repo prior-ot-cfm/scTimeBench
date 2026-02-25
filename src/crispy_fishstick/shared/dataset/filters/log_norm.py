ff64d09ca752ac6246349cea1afb73024fd1b387
Eric Huang <eric.haoran.huang@gmail.com>
2026-02-25 13:57:36 -0500

    [fix] Fix log norm to use X if it's raw counts
    Also add in default datasets and others to be good.

--- src/crispy_fishstick/shared/dataset/filters/log_norm.py
+++ src/crispy_fishstick/shared/dataset/filters/log_norm.py
@@ -31,20 +31,31 @@ class LogNormFilter(BaseDatasetFilter):
             )
             return ann_data
 
-        if ann_data.raw is None:
+        is_x_raw = ann_data.X.max() > self._parameters()["counts"]
+        if not is_x_raw and ann_data.raw is None:
             raise ValueError(
                 "Data appears to be normalized some other way and does not provide raw data. This could lead to errors down the line "
                 "with cell type lineage prediction. Please ensure that the data is log-normalized to counts=10^4, or that the raw data is provided for proper CellTypist performance."
             )
 
         # we want all the vars, etc. to stay the same, so we just replace X with the raw data
-        data = ann_data.raw.to_adata()
+        if not is_x_raw:
+            logging.debug(
+                "Data appears to be log-transformed but not normalized. Using raw data for normalization."
+            )
+            data = ann_data.raw.to_adata()
+        else:
+            logging.debug(
+                "Data appears to be raw counts. Normalizing to counts=10^4 and log-transforming."
+            )
+            data = ann_data.copy()
+
         sc.pp.filter_genes(
             data, min_cells=1
         )  # filter out genes that are not expressed in any cells
         sc.pp.normalize_total(data, target_sum=self._parameters()["counts"])
         sc.pp.log1p(data)
-        print(
-            f'Finished running normalization, check {is_log_normalized_to_counts(data, counts=self._parameters()["counts"])}'
+        logging.debug(
+            f'Finished running normalization, final check: {is_log_normalized_to_counts(data, counts=self._parameters()["counts"])}'
         )
         return data
